41f5e1bffcd05c0037a6d72be69a807cf4967598
Dustin Schau <dustinschau@gmail.com>
2019-02-20 14:08:50 -0600

    chore: do not use variable

--- src/index.js
+++ src/index.js
@@ -2,21 +2,19 @@ import React from 'react'
 import { useStaticQuery, graphql } from 'gatsby'
 
 function Meta() {
-  const data = useStaticQuery(query)
+  const data = useStaticQuery(graphql`
+    {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
 
   return (
     <h1>{data.site.siteMetadata.title}</h1>
   )
 }
-
-const query = graphql`
-  {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-  }
-`
 
 export default Meta
